8420cc7858929dcfe063cc4238cd7ff616545bdc
seosieve <royalcircle97@naver.com>
2024-09-02 09:48:15 +0900

    Initial Commit

---
+++ SwiftUINetwork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUINetwork
+//
+//  Created by 서충원 on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUINetwork/SwiftUINetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINetworkApp.swift
+//  SwiftUINetwork
+//
+//  Created by 서충원 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
